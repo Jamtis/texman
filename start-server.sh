0657a92f0d5de10c399e1bd956168bf4b205d397
Nicholas Brandt <nicholas.brandt@inf.ethz.ch>
2024-03-04 19:52:18 +0100

    disable workspace trust

--- start-server.sh
+++ start-server.sh
@@ -1,2 +1,4 @@
 # code-server --bind-addr=0.0.0.0:8080 --auth=none --disable-telemetry --cert
-code-server --bind-addr=0.0.0.0:8080 --auth=password --disable-telemetry --cert
+USER_DATA_DIR=/usr/user_data
+mkdir -p $USER_DATA_DIR
+code-server --bind-addr=0.0.0.0:8080 --auth=password --disable-telemetry --cert --disable-workspace-trust --user-data-dir=$USER_DATA_DIR
