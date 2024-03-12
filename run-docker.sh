28f5d972755947496a92b3d1638cf2abda6425fa
Nicholas Brandt <nicholas.brandt@inf.ethz.ch>
2024-03-12 08:18:00 +0100

    mount settings

--- run-docker.sh
+++ run-docker.sh
@@ -14,6 +14,13 @@ if docker ps -a --format '{{.Names}}' | grep -q "^$CONTAINER_NAME$"; then
 	docker rm $CONTAINER_NAME
 fi
 
-docker create --name $CONTAINER_NAME --net=host -e "PASSWORD=$PASSWORD" -v $(readlink -f ../projects):/workdir nicholasbrandt/texdocker
+EXT_PROJECTS_FOLDER=$(readlink -f ../projects)
+mkdir -p $EXT_PROJECTS_FOLDER
+INT_PROJECTS_FOLDER=/workdir
+EXT_SETTINGS_FOLDER=$(readlink -f ../.settings)
+mkdir -p $EXT_PROJECTS_FOLDER
+INT_SETTINGS_FOLDER=/root/.local/share/code-server/User
+
+docker create --name $CONTAINER_NAME --net=host -e "PASSWORD=$PASSWORD" -v $EXT_PROJECTS_FOLDER:$INT_PROJECTS_FOLDER -v $EXT_SETTINGS_FOLDER:$INT_SETTINGS_FOLDER nicholasbrandt/texdocker
 
 docker restart $CONTAINER_NAME
