72a1efff98cb98b5f32f447b36e3665e96ca8a6c
Nicholas Brandt <nicholas.brandt@inf.ethz.ch>
2024-03-21 10:07:14 +0100

    add stop command to save container state

--- start-server.sh
+++ start-server.sh
@@ -1,4 +1,4 @@
 # code-server --bind-addr=0.0.0.0:8080 --auth=none --disable-telemetry --cert
 USER_DATA_DIR=/usr/user_data
 mkdir -p $USER_DATA_DIR
-code-server --bind-addr=0.0.0.0:8080 --auth=password --disable-telemetry --cert --disable-workspace-trust --user-data-dir=$USER_DATA_DIR
+code-server --bind-addr=0.0.0.0:8080 --auth=password --disable-telemetry --cert --disable-workspace-trust --user-data-dir=$USER_DATA_DIR --disable-workspace-trust --disable-getting-started-override --install-extension 'James-Yu.latex-workshop'
